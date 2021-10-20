d95adf80e88e0459407f6b7548f31f550864222f
MarcusMoen <marcus.theodor.moen@gmail.com>
2021-10-20 16:48:41 +0200

    Second push

--- PenningTrap.cpp
+++ PenningTrap.cpp
@@ -35,10 +35,12 @@ arma::vec PenningTrap::external_B_field(arma::vec r){
 
 // The total force on particle_i from the external fields
 arma::vec PenningTrap::total_force_external(int i){
-  arma::vec cross_prod = {particle_l[i].charge*B0_*particle_l[i].velocity[1], particle_l[i].charge*B0_*particle_l[i].velocity[0], 0};  // MOST LIKELY ERROR HERE
+  arma::vec cross_prod = {particle_l[i].charge*B0_*particle_l[i].velocity[1], -particle_l[i].charge*B0_*particle_l[i].velocity[0], 0};
+  //cout<< cross_prod;
+  arma::vec ma_kalles_noe = {particle_l[i].position[0], particle_l[i].position[1], -2*particle_l[i].position[2]};
 
-  arma::vec F_ex = particle_l[i].charge * external_E_field(particle_l[i].position) + cross_prod;  // MOST LIKELY ERROR HERE
-
+  arma::vec F_ex = particle_l[i].charge *(V0_ / pow(d_,2))*ma_kalles_noe  + cross_prod;
+  //cout << F_ex << "\n";
   return F_ex;
 }
 
@@ -50,6 +52,7 @@ arma::vec PenningTrap::total_force_particles(int i){
   arma::vec F_part = {k_e, k_e, k_e};
   if(particle_l.size()==1){
     F_part = {0,0,0};
+
   }else{
     for(int j=0; j<particle_l.size(); j++){
       F_part += particle_l[j].charge * ((particle_l[i].position - particle_l[j].position) / pow(abs(particle_l[i].position - particle_l[j].position), 3));
@@ -73,7 +76,7 @@ void PenningTrap::evolve_RK4(double dt){
 
   arma::mat k1x(n, 3), k2x(n, 3), k3x(n, 3), k4x(n, 3), k1v(n, 3), k2v(n, 3), k3v(n, 3), k4v(n, 3);
 
-  //arma::vec original_particle_l = particle_l;
+  std::vector<Particle> original_particle_l = particle_l;
 
   for(int i = 0; i < n; i++){
     a[i,0] = total_force(i)[0] / particle_l[i].mass;
@@ -85,6 +88,7 @@ void PenningTrap::evolve_RK4(double dt){
   for(int j = 0; j < n; j++){
     for(int i=0; i<3; i++){
       k1v[j,i] = a[j,i] * dt;
+      //cout << k1v[j,i];
       k1x[j,i] = particle_l[j].velocity[i] * dt;
 
       particle_l[j].velocity[i] += k1v[j,i]/2;
@@ -142,38 +146,25 @@ void PenningTrap::evolve_RK4(double dt){
 
       for(int j = 0; j < n; j++){
           for(int i=0; i < 3; i++){
-            particle_l[j].velocity[i] += 1/6*(k1v[j,i]+2*k1v[j,i]+2*k3v[j,i]+k4v[j,i]);
-            particle_l[j].position[i] += 1/6*(k1x[j,i]+2*k1x[j,i]+2*k3x[j,i]+k4x[j,i]);
+            particle_l[j].velocity[i] = original_particle_l[j].velocity[i] + 1./6*(k1v[j,i]+2*k2v[j,i]+2*k3v[j,i]+k4v[j,i]);
+            //cout << 1/6*(k1v[j,i]+2*k2v[j,i]+2*k3v[j,i]+k4v[j,i]);
+            particle_l[j].position[i] = original_particle_l[j].position[i] + 1./6*(k1x[j,i]+2*k2x[j,i]+2*k3x[j,i]+k4x[j,i]);
           }
         }
 }
 
 
-//void PenningTrap::evolve_RK4(double dt){
-//  arma::mat a;
-//  int n = p_.size();
-//  for(int i = 0; i < n; i++){
-//    a(i) = total_force(i) / p_(i).m;
-//  }
-//
-//  double k1, k2, k3, k4;
-//  for(int j = 0; j < n; j++){
-//    for(int k=0;k<3;k++){
-//      k1 = dt*a(j,k);
-//      k2 = dt/2*a(j,k)
-//    }
-//  }
-//}
+
 
 
 void PenningTrap::evolve_forward_Euler(double dt){
-  arma::mat a(particle_l.size(), 3);
   int n = particle_l.size();
+  arma::mat a(n, 3);
   for(int i = 0; i < n; i++){
-    arma::vec tf = total_force(i);
-    a[i,0] = tf[0] / particle_l[i].mass;
-    a[i,1] = tf[1] / particle_l[i].mass;
-    a[i,2] = tf[2] / particle_l[i].mass;
+    //arma::vec tf = total_force(i);
+    a[i,0] = total_force(i)[0] / particle_l[i].mass;
+    a[i,1] = total_force(i)[1] / particle_l[i].mass;
+    a[i,2] = total_force(i)[2] / particle_l[i].mass;
   }
 
   for(int j = 0; j < n; j++){
--- main_project3.cpp
+++ main_project3.cpp
@@ -7,6 +7,13 @@
 #include <vector>
 #include <string>
 
+void analytical_solution(arma::vec& x, arma::vec& y, arma::vec& z, double dt, double w_z, double w_p, double w_m, double A_p, double A_m){
+  for(int i=1; i<x.size(); i++){
+    x[i] = A_p*cos(w_m*i*dt) + A_p*cos(w_m*i*dt);
+    y[i] = A_p*sin(w_m*i*dt) + A_p*sin(w_m*i*dt);
+    z[i] = 1./2 * (z[0]*exp(w_z*(i*dt)) + z[0]*exp(-w_z*(i*dt)));
+  }
+}
 
 
 
@@ -20,20 +27,97 @@ int main(){
     double d = pow(10,4);
     double V_d_ratio = 9.65;
 
-    int q = 1;
-    double m = 4.077;
-    arma::vec r = {1., 1., 1.};
-    arma::vec v = {2., 1., 3.};
+    //One particle
+    int q = 1.;
+    double m = 40.078;
+    arma::vec r = {1., 0., 1.};
+    arma::vec v = {0., 1., 0.};
     vector<Particle> pl;
     Particle singly_charged_Calcium = Particle(q, m, r, v);
     PenningTrap trap = PenningTrap(B_0, V_0, d, pl);
     trap.add_particle(singly_charged_Calcium);
+    double dt = 0.001;
 
-    std::cout << singly_charged_Calcium.charge << singly_charged_Calcium.mass << singly_charged_Calcium.position;
-    //std::cout << trap.particle_l[0].position;
-    trap.evolve_RK4(0.1);
-    std::cout << trap.particle_l[0].position;
-    //trap.evolve_forward_Euler(0.1);
-    //trap.evolve_forward_Euler(0.1);
+
+    //Start analytical solution part
+    double w_0 = singly_charged_Calcium.charge*B_0/singly_charged_Calcium.mass;
+    double w_z = sqrt(2*singly_charged_Calcium.charge*V_0/(singly_charged_Calcium.mass*pow(d,2)));
+
+    double w_p = (w_0 + sqrt(pow(w_0,2)-2*pow(w_z,2)))/2;
+    double w_m = (w_0 - sqrt(pow(w_0,2)-2*pow(w_z,2)))/2;
+
+    arma::vec x(int(100/dt)+1);
+    x(0) = trap.particle_l[0].position[0];
+    arma::vec y(int(100/dt)+1);
+    y(0) = trap.particle_l[0].position[1];
+    arma::vec z(int(100/dt)+1);
+    z(0) = trap.particle_l[0].position[2];
+
+    double A_p = (1+w_m*1.)/(w_m - w_p);
+    double A_m = (1+w_p*1.)/(w_m - w_p);
+
+    analytical_solution(x, y, z, dt, w_z, w_p, w_m, A_p, A_m);
+
+    std::ofstream myfile_an;
+    myfile_an.open("coordinates_an.txt");
+    for(int i=0; i<x.size(); i++){
+      myfile_an << std::scientific << dt*(i) << " " << std::scientific << x[i] << " " << std::scientific << y[i] <<" " << std::scientific << z[i] << "\n";
+    }
+    //End analytical solution part
+
+
+    //Start RK4 solution part
+    std::ofstream myfile;
+    myfile.open("coordinates_rk.txt");
+    myfile << std::scientific << 0 << " " << std::scientific << trap.particle_l[0].position[0] << " " << std::scientific << trap.particle_l[0].position[1] <<" " << std::scientific << trap.particle_l[0].position[2] << "\n";
+    for(int i=0; i<int(100/dt); i++){
+      trap.evolve_RK4(dt);
+      myfile << std::scientific << dt*(i+1) << " " << std::scientific << trap.particle_l[0].position[0] << " " << std::scientific << trap.particle_l[0].position[1] <<" " << std::scientific << trap.particle_l[0].position[2] << "\n"; //Write the number of iteration needed for convergence to file
+    }
+    //End RK4 solution part
+
+    //Start euler solution part
+    q = 1.;
+    m = 40.078;
+    r = {1., 0., 1.};
+    v = {0., 1., 0.};
+    vector<Particle> pl2;
+    Particle singly_charged_Calcium2 = Particle(q, m, r, v);
+    PenningTrap trap2 = PenningTrap(B_0, V_0, d, pl2);
+    trap2.add_particle(singly_charged_Calcium2);
+
+    std::ofstream myfile2;
+    myfile2.open("coordinates_euler.txt");
+    myfile2 << std::scientific << 0 << " " << std::scientific << trap2.particle_l[0].position[0] << " " << std::scientific << trap2.particle_l[0].position[1] <<" " << std::scientific << trap2.particle_l[0].position[2] << "\n";
+    dt = 0.001;
+    for(int i=0; i<int(100/dt); i++){
+      trap2.evolve_forward_Euler(dt);
+      myfile2 << std::scientific << dt*(i+1) << " " << std::scientific << trap2.particle_l[0].position[0] << " " << std::scientific << trap2.particle_l[0].position[1] <<" " << std::scientific << trap2.particle_l[0].position[2] << "\n"; //Write the number of iteration needed for convergence to file
+    }
+    //End euler solution part
+
+
+
+
+    //Two particles:
+    arma::vec r2 = {2., 0.3, 1.2};
+    arma::vec v2 = {-0.3, 1., 0.2};
+    std::vector<Particle> pl2particles;
+    Particle singly_charged_Calcium_nr2 = Particle(q, m, r2, v2);
+    PenningTrap trap2particles = PenningTrap(B_0, V_0, d, pl2particles);
+    trap2particles.add_particle(singly_charged_Calcium);
+    trap2particles.add_particle(singly_charged_Calcium_nr2);
+
+    std::ofstream myfile2particles;
+    myfile2particles.open("coordinates_rk2particles.txt");
+    myfile2particles << std::scientific << 0 << " " << std::scientific << trap2particles.particle_l[0].position[0] << " " << std::scientific << trap2particles.particle_l[0].position[1] <<" " << std::scientific << trap2particles.particle_l[0].position[2] << " " << std::scientific << trap2particles.particle_l[1].position[0] << " " << std::scientific << trap2particles.particle_l[1].position[1] <<" " << std::scientific << trap2particles.particle_l[1].position[2] << "\n";
+    for(int i=0; i<int(100/dt); i++){
+      trap2particles.evolve_RK4(dt);
+      myfile2particles << std::scientific << dt*(i+1) << " " << std::scientific << trap2particles.particle_l[0].position[0] << " " << std::scientific << trap2particles.particle_l[0].position[1] <<" " << std::scientific << trap2particles.particle_l[0].position[2] << " " << std::scientific << trap2particles.particle_l[1].position[0] << " " << std::scientific << trap2particles.particle_l[1].position[1] <<" " << std::scientific << trap2particles.particle_l[1].position[2] << "\n"; //Write the number of iteration needed for convergence to file
+    }
+
+
+
+
     return 0;
 }
